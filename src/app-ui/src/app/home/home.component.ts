c412da195f359c938d304d39d2190d02215073b3
Pawel Bialas <p.cz.bialas@gmail.com>
2019-07-29 00:12:39 +0200

    date format for post window

--- src/app-ui/src/app/home/home.component.ts
+++ src/app-ui/src/app/home/home.component.ts
@@ -20,5 +20,49 @@ export class HomeComponent implements OnInit {
     })
   }
 
+  formatJoinDate(date: any): String {
+    let stringDate: String = date;
+    let year = stringDate.substr(0,4);
+    let month = stringDate.substr(5,2);
 
+    if (month === '01') {
+      return ' January ' + year;
+    }
+    if (month === '02') {
+      return ' February ' + year;
+    }
+    if (month === '03') {
+      return ' March ' + year;
+    }
+    if (month === '04') {
+      return ' April ' + year;
+    }
+    if (month === '05') {
+      return ' May ' + year;
+    }
+    if (month === '06') {
+      return ' June ' + year;
+    }
+    if (month === '07') {
+      return ' July ' + year;
+    }
+    if (month === '08') {
+      return ' August ' + year;
+    }
+    if (month === '09') {
+      return ' September ' + year;
+    }
+    if (month === '10') {
+      return ' October ' + year;
+    }
+    if (month === '11') {
+      return ' November ' + year;
+    }
+    if (month === '12') {
+      return ' December ' + year;
+    }
+
+
+
+  }
 }
