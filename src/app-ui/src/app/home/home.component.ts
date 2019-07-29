2b80d0b07b2426576f07ea15f6ad3d5a7be5a5c6
Pawel Bialas <p.cz.bialas@gmail.com>
2019-07-29 03:00:15 +0200

    date formaters added for post display

--- src/app-ui/src/app/home/home.component.ts
+++ src/app-ui/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginAuthService} from "../authentication/login-auth.service";
 import {PostService} from "../shared/service/post.service";
+import {MonthFormat} from "../shared/moth-format/month-format";
 
 
 @Component({
@@ -21,48 +22,33 @@ export class HomeComponent implements OnInit {
   }
 
   formatJoinDate(date: any): String {
-    let stringDate: String = date;
-    let year = stringDate.substr(0,4);
-    let month = stringDate.substr(5,2);
 
-    if (month === '01') {
-      return ' January ' + year;
-    }
-    if (month === '02') {
-      return ' February ' + year;
-    }
-    if (month === '03') {
-      return ' March ' + year;
-    }
-    if (month === '04') {
-      return ' April ' + year;
-    }
-    if (month === '05') {
-      return ' May ' + year;
-    }
-    if (month === '06') {
-      return ' June ' + year;
-    }
-    if (month === '07') {
-      return ' July ' + year;
-    }
-    if (month === '08') {
-      return ' August ' + year;
-    }
-    if (month === '09') {
-      return ' September ' + year;
-    }
-    if (month === '10') {
-      return ' October ' + year;
-    }
-    if (month === '11') {
-      return ' November ' + year;
-    }
-    if (month === '12') {
-      return ' December ' + year;
+    let joinDate: Date = new Date(date);
+    let month = MonthFormat.monthFormat(joinDate);
+    let year = joinDate.getFullYear();
+
+    return month + ' ' + year;
+
+  }
+
+  formatCreationDate(date: any): String {
+    let publicationDate: Date = new Date(date);
+    let time: number = Date.now() - publicationDate.getTime();
+
+    if (time > 86400000) {
+      return ' ' + publicationDate.getDay() + ' ' + MonthFormat.monthFormat(publicationDate) + ' ' + publicationDate.getFullYear();
+    } 
+    
+    if (time > 3600000) {
+      let hours = time / 3600000;
+      let hoursFormatted = Math.round(hours);
+      return ' ' + hoursFormatted + ' h ago';
     }
 
-
-
+    if (time < 3600000) {
+      let minutes = time / 60000;
+      let minutesFormatted = Math.round(minutes);
+      return ' ' + minutesFormatted + ' min ago';
+    }
   }
 }
