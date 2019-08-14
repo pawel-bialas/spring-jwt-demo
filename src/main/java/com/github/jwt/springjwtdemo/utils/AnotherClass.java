ee8d3bf5ca2efabc474feaedf2791e92e8276e99
Pawel Bialas <p.cz.bialas@gmail.com>
2019-08-15 00:04:30 +0200

    minor refactor

---
+++ src/main/java/com/github/jwt/springjwtdemo/utils/AnotherClass.java
@@ -0,0 +1,15 @@
+package com.github.jwt.springjwtdemo.utils;
+
+import java.util.Random;
+
+public class AnotherClass {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+
+        System.out.println(r.nextInt(10));
+
+    }
+
+}
